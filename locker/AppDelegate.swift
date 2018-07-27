193eb4ddb4b993fd55ca1d2a9d5bdda97319168b
Thomas Schulze <thomas.schulze@outraxx.de>
2018-07-27 14:47:19 +0200

    Different Lock-Modes, no window anymore

--- locker/AppDelegate.swift
+++ locker/AppDelegate.swift
@@ -17,33 +17,45 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
     @IBOutlet weak var setup: NSWindow!
     @IBOutlet weak var mQuit: NSMenuItem!
-    @IBOutlet weak var lockFrom: NSTextField!
-    @IBOutlet weak var unlockFrom: NSTextField!
-    @IBOutlet weak var macAddress: NSTextField!
-    @IBOutlet weak var password: NSSecureTextField!
-
-
+    @IBOutlet weak var lockModeMenu: NSMenu!
+    
+    @IBOutlet weak var password: NSSecureTextFieldCell!
+    @IBOutlet weak var macAddress: NSTextFieldCell!
+    
     
     @IBOutlet weak var rssiLock: NSMenu!
-    
+    @IBOutlet weak var rssiUnlock: NSMenu!
     
     let statusItem = NSStatusBar.system.statusItem(withLength: -1)
     let defaults = UserDefaults.standard
-    var locked = false
-    var ready = false
-    var average = 0
+
+    var counter = 0
     var rssi = 0
-    var lockValue = -70
-    var unlockValue = -70
-    var address = ""
-    var unlockPassword = ""
+    var scriptObject : NSAppleScript?
+    var timer = Timer()
+    
     var canLock = false
     var canunlock = false
-
+    var locked = false
+    var ready = false
+    var manualUnlock = false
+    
+    /* Default Values */
+    var lockMode = 0 // 0=off, 1=lock, 2=Screensaver
+    var lockValue = -70
+    var unlockValue = -65
+    var address = ""
+    var unlockPassword = ""
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        for i in stride(from: 70, to: 30, by: -5) {
+        
+        let center = DistributedNotificationCenter.default()
+        center.addObserver(self, selector: #selector(AppDelegate.screenLocked), name: NSNotification.Name(rawValue: "com.apple.screenIsLocked"), object: nil)
+        center.addObserver(self, selector: #selector(AppDelegate.screenUnlocked), name: NSNotification.Name(rawValue: "com.apple.screenIsUnlocked"), object: nil)
+        
+        for i in stride(from: 100, to: 30, by: -5) {
             let editMenuItem = NSMenuItem()
             editMenuItem.title = "-"+String(i)+" dBm"
             editMenuItem.action = #selector(setLockRssi(_:))
@@ -52,45 +64,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("lock"))
-            button.action = #selector(AppDelegate.displayRSSI(_:))
+            //button.action = #selector(AppDelegate.displayRSSI(_:))
             //button.sendAction(on: [.leftMouseUp, .rightMouseUp])
             //button.target = self
         }
         statusItem.menu = statusMenu
         loadData()
+        buildUnlockMenu()
 
         if !self.macAddress.stringValue.isEmpty && !self.password.stringValue.isEmpty {
-            self.setup!.orderOut(self)
             self.ready = true
-            
         }
-
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.displayRSSI), userInfo: nil, repeats: true)
         DispatchQueue.global(qos: .utility).async {
             print("This is run on the background queue")
             // Get the Device
             if self.ready==true {
+                
                 guard let bluetoothDevice = IOBluetoothDevice(addressString: self.address) else {
                     print("Device not found")
                     return
                 }
                 while 1==1 {
                     if bluetoothDevice.isConnected()==false {
-                        print("Device not connected")
+                        //print("Device not connected")
                         bluetoothDevice.openConnection()
-                        
                     }
                     else {
                         self.rssi = Int(bluetoothDevice.rawRSSI())
                         if self.rssi<0 {
                             if self.rssi < self.lockValue {
-                                self.average=self.average+1
-                                if self.average==10 {
+                                self.counter=self.counter+1
+                                if self.counter==10 {
                                     self.canLock=true
                                     self.canunlock=false
                                 }
                             }
-                            else {
-                                self.average=0
+                            else if self.rssi >= self.unlockValue {
+                                self.counter=0
                                 self.canLock=false
                                 self.canunlock=true
                             }
@@ -100,27 +111,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                         print("\(self.rssi) => \(self.lockValue) => \(self.unlockValue)")
                         print(self.canLock)
                         print("-----------------")
+                        print(self.rssi)
+                        sleep(1)
+ 
+
+                        print("\(self.rssi) => \(self.locked) => \(self.manualUnlock) => \(self.canLock) => \(self.canunlock)")
                         sleep(1)
                         */
+                        
                         if self.canLock {
-                            if self.locked==false {
+                            if self.locked==false && self.manualUnlock==false{
                                 print("LOCK")
-                                self.shell("/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession", "-suspend")
+                                self.getOutRangeScript()
                                 self.locked=true
                                 sleep(3)
                             }
                         }
                         else if self.canunlock {
-                            if self.locked==true {
+                            if self.locked==true && !self.manualUnlock {
                                 print("UNLOCK")
-                                
+                                if self.lockMode==2 {
+                                    keysend(0x24, useCommandFlag: false)
+                                    sleep(1)
+                                }
                                 for i in self.unlockPassword {
                                     keysend(keymap[i]!, useCommandFlag: false)
                                 }
                                 keysend(0x24, useCommandFlag: false)
-                                
                                 self.locked=false
                                 sleep(3)
+                            }
+                            else {
+                                self.manualUnlock = false
                             }
                         }
                     }
@@ -128,24 +150,81 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
-    
-    @IBAction func closeSetup(_ sender: Any) {
-        defaults.set(self.macAddress.stringValue, forKey: "address")
-        defaults.set(self.password.stringValue, forKey: "password")
-        
-        //defaults.set(self.unlock.integerValue, forKey: "unlock")
-        if !self.macAddress.stringValue.isEmpty && !self.password.stringValue.isEmpty {
-            self.ready=true
+
+    func buildUnlockMenu() {
+        rssiUnlock.removeAllItems()
+        let start=abs(Int(self.lockValue))-5
+        var checked = false
+        for i in stride(from: start, to: 30, by: -5) {
+            let editMenuItem = NSMenuItem()
+            editMenuItem.title = "-"+String(i)+" dBm"
+            editMenuItem.action = #selector(setUnlockRssi(_:))
+            if checked==false {
+                if self.unlockValue < (0-start) && i==start{
+                    editMenuItem.state = NSControl.StateValue.on
+                    self.unlockValue = 0-i
+                    checked = true
+                }
+                else if self.unlockValue==0-i {
+                    editMenuItem.state = NSControl.StateValue.on
+                    checked = true
+                }
+            }
+            rssiUnlock.addItem(editMenuItem)
         }
-        loadData()
-        self.setup!.orderOut(self)
+    }
+
+    func getOutRangeScript() {
+        var myAppleScript=""
+        if self.lockMode>0 {
+            if self.lockMode>1 {
+                myAppleScript = "tell application \"System Events\" \n" +
+                    "start current screen saver \n" +
+                "end tell \n"
+                var error: NSDictionary?
+                NSAppleScript(source: myAppleScript)?.executeAndReturnError(&error)
+            }
+            else {
+                self.shell("/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession", "-suspend")
+            }
+            
+        }
+        return
     }
     
-
+    func getInRangeScript() {
+        if self.lockMode>0 {
+            let myAppleScript = "on run\n"
+                //+ "tell application \"/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app\" to quit\n"
+                + "tell application \"System Events\" to keystroke return\n"
+                + "delay 2.0\n"
+                + "tell application \"System Events\" to keystroke \"" + (self.unlockPassword) + "\"\n"
+                + "delay 0.5\n"
+                + "tell application \"System Events\" to keystroke return\n"
+                + "end run\n"
+            var error: NSDictionary?
+            NSAppleScript(source: myAppleScript)?.executeAndReturnError(&error)
+        }
+        return
+    }
+    
+    @objc func screenLocked(_ sender: Any) {
+        print("Manual Lock")
+    }
    
-    @objc func displayRSSI(_ sender: Any?) {
-        print("*****")
-        self.mRssi.title="Rssi : "+String(self.rssi)
+    @objc func screenUnlocked(_ sender: Any) {
+        print("Manual unlock")
+        self.manualUnlock=true
+        self.locked=false
+    }
+    
+    @objc func displayRSSI() {
+        if self.rssi<0 {
+            self.mRssi.title="Rssi : "+String(self.rssi)
+        }
+        else {
+            self.mRssi.title="Not Connected"
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -153,24 +232,67 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @objc func setLockRssi(_ sender: NSMenuItem?) {
-        var val = sender?.title.replacingOccurrences(of: " dBm", with: "")
-        print(val)
+        let val = sender?.title.replacingOccurrences(of: " dBm", with: "")
         sender?.state = NSControl.StateValue.on
-        var checked = rssiLock.item(withTitle: String(self.lockValue)+" dBm")
+        let checked = rssiLock.item(withTitle: String(self.lockValue)+" dBm")
         checked?.state = NSControl.StateValue.off
         self.lockValue = Int(val!)!
         defaults.set(self.lockValue, forKey: "lock")
+        buildUnlockMenu()
+    }
+    
+    @objc func setUnlockRssi(_ sender: NSMenuItem?) {
+        let val = sender?.title.replacingOccurrences(of: " dBm", with: "")
+        sender?.state = NSControl.StateValue.on
+        let checked = rssiUnlock.item(withTitle: String(self.unlockValue)+" dBm")
+        checked?.state = NSControl.StateValue.off
+        self.unlockValue = Int(val!)!
+        defaults.set(self.unlockValue, forKey: "unlock")
     }
     
     @IBAction func qAction(_ sender: Any) {
         NSApplication.shared.terminate(self)
     }
-
-    @IBAction func sAction(_ sender: Any) {
-        self.setup!.orderFront(self)
-        
-
+    
+    @IBAction func passwordChanged(_ sender: NSSecureTextFieldCell) {
+        self.ready=false
+        self.unlockPassword = sender.stringValue
+        defaults.set(self.unlockPassword, forKey: "password")
+        if !self.macAddress.stringValue.isEmpty && !self.password.stringValue.isEmpty {
+            self.ready=true
+        }
     }
+    
+    @IBAction func macAddressChanged(_ sender: NSTextFieldCell) {
+        self.ready=false
+        self.address = sender.stringValue
+        defaults.set(self.address, forKey: "address")
+        if !self.macAddress.stringValue.isEmpty && !self.password.stringValue.isEmpty {
+            self.ready=true
+        }
+    }
+    
+    @IBAction func lockOff(_ sender: NSMenuItem?) {
+        lockModeMenu.item(at: self.lockMode)?.state = NSControl.StateValue.off
+        self.lockMode = 0
+        sender?.state = NSControl.StateValue.on
+        defaults.set(self.lockMode, forKey: "lockMode")
+    }
+    
+    @IBAction func lockLock(_ sender: NSMenuItem?) {
+        lockModeMenu.item(at: self.lockMode)?.state = NSControl.StateValue.off
+        self.lockMode = 1
+        sender?.state = NSControl.StateValue.on
+        defaults.set(self.lockMode, forKey: "lockMode")
+    }
+    
+    @IBAction func lockSaver(_ sender: NSMenuItem?) {
+        lockModeMenu.item(at: self.lockMode)?.state = NSControl.StateValue.off
+        self.lockMode = 2
+        sender?.state = NSControl.StateValue.on
+        defaults.set(self.lockMode, forKey: "lockMode")
+    }
+    
     
     func loadData() {
         let address = defaults.string(forKey: "address")
@@ -178,26 +300,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let password = defaults.string(forKey: "password")
         self.password.stringValue = password ?? ""
         let lock = defaults.integer(forKey: "lock")
-        //self.lock.integerValue = lock ?? self.lockValue
-        //let unlock = defaults.integer(forKey: "unlock")
-        //self.unlock.integerValue = unlock ?? self.unlockValue
+        let unlock = defaults.integer(forKey: "unlock")
+        let mode = defaults.integer(forKey: "lockMode")
         
         var checked = rssiLock.item(withTitle: String(lock)+" dBm")
         checked?.state = NSControl.StateValue.on
+        checked = rssiUnlock.item(withTitle: String(unlock)+" dBm")
+        checked?.state = NSControl.StateValue.on
         
         
+        
+        self.unlockValue = unlock
         self.lockValue = lock
         self.address = self.macAddress.stringValue
         self.unlockPassword = self.password.stringValue
+        self.lockMode = mode
+        lockModeMenu.item(at: self.lockMode)?.state = NSControl.StateValue.on
     }
     
-    func shell(_ args: String...) -> Int32 {
+    func shell(_ args: String...) {
         let task = Process()
         task.launchPath = "/usr/bin/env"
         task.arguments = args
         task.launch()
         task.waitUntilExit()
-        return task.terminationStatus
     }
     
 }
